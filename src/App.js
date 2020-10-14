f66968de1d9626fb7f398faf7cbc12228408d7a3
mekatina65 <rlbengineer@gmail.com>
2020-10-14 16:31:13 -0700

    first revise

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
-      <p>hello mars</p>
+      <p>hello mars anf from neptune</p>
       <h2>hello venusnpm start</h2>
     </div>
   );
