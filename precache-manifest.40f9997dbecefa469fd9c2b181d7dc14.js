a874dffd231670b52f2c999c21652b71d377a617
mekatina65 <rlbengineer@gmail.com>
2020-10-14 16:19:09 -0700

    Updates

---
+++ precache-manifest.40f9997dbecefa469fd9c2b181d7dc14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66acb0d0f51960fb8fc339056471f8c",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "1ebc13e76d73d0069309",
+    "url": "/react-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b6ad9b6c71bb6339f872",
+    "url": "/react-deploy/static/js/2.06e867cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.06e867cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ebc13e76d73d0069309",
+    "url": "/react-deploy/static/js/main.a099d4d1.chunk.js"
+  },
+  {
+    "revision": "d711428e33080d9e0104",
+    "url": "/react-deploy/static/js/runtime-main.3f71107b.js"
+  }
+]);
