94aba6f25ed611c982f35312d5e08ee593979d68
mekatina65 <rlbengineer@gmail.com>
2020-10-14 16:15:40 -0700

    react repo commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <p>hello mars</p>
+      <h2>hello venusnpm start</h2>
+    </div>
+  );
+}
+
+export default App;
